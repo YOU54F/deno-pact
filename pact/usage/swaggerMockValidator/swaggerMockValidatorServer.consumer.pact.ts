ed67cfdb7fa5eaafeaaea481eefa1fb94c96bfd9
Yousaf Nabi <you@saf.dev>
2022-12-06 17:45:00 +0000

    test: add swaggerMockValidatorServer consumer test

---
+++ pact/usage/swaggerMockValidator/swaggerMockValidatorServer.consumer.pact.ts
@@ -0,0 +1,179 @@
+import { getModuleDir } from "../../lib/utils.ts";
+import * as path from "https://deno.land/std/path/mod.ts";
+import { DenoPact } from "../../denoPact.ts";
+import { PactFfi as Pact } from "../../lib/types.ts";
+import {
+  beforeAll,
+  describe,
+  expect,
+  it,
+  run
+} from "https://deno.land/x/tincan/mod.ts";
+const decoder = new TextDecoder("utf-8");
+
+describe("Swagger Mock Validator Service", () => {
+  let oasDefinition: any;
+  let pactFile: any;
+
+  beforeAll(async () => {
+    oasDefinition = JSON.parse(
+      decoder.decode(
+        await Deno.readFile(path.join(getModuleDir(import.meta), "oas.json"))
+      )
+    );
+    pactFile = JSON.parse(
+      decoder.decode(
+        await Deno.readFile(path.join(getModuleDir(import.meta), "pact.json"))
+      )
+    );
+  });
+
+  describe("Validations", () => {
+    it("should return ok if there are no errors", async () => {
+      const expected = {
+        errors: [],
+        success: true,
+        warnings: [],
+        consumer: "ExampleConsumer",
+        provider: "ExampleProvider",
+        specContentPathOrUrl: "content",
+        mockContentPathOrUrl: "content"
+      };
+
+      const requestData = {
+        oas: {
+          content: oasDefinition
+        },
+        pact: {
+          content: pactFile
+        }
+      };
+
+      const denoPact = new DenoPact();
+      await denoPact
+        .setupLoggers(Pact.LevelFilter.LevelFilter_Info)
+        // Arrange
+        .newPact(
+          "swagger-mock-validator-consumer",
+          "swagger-mock-validator-provider"
+        )
+        .addMetaDataToPact(denoPact.getPactFfiVersion())
+        .newInteraction("A OpenAPI/Pact request")
+        .given("an OpenAPI definition exists")
+        .uponReceiving("an HTTP request that exists in the OpenAPI definition")
+        .setPactSpecification(Pact.PactSpecification.PactSpecification_V4)
+        .withRequest("/", "POST")
+        .withResponse(200)
+        .withHeader(
+          Pact.InteractionPart.InteractionPart_Request,
+          "content-type",
+          "application/json"
+        )
+        .withBody(
+          Pact.InteractionPart.InteractionPart_Request,
+          "application/json",
+          requestData
+        )
+        .withBody(
+          Pact.InteractionPart.InteractionPart_Response,
+          "application/json",
+          expected
+        )
+        .createMockServerForTransport("http", "0.0.0.0", 0)
+
+        .executeTest(async () => {
+          // Act
+          const response = await fetch(
+            `http://localhost:${denoPact.getMockServerPort()}`,
+            {
+              method: "POST",
+              body: JSON.stringify(requestData),
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              }
+            }
+          );
+          // Assert
+          const actual = await response.json();
+          expect(actual).toEqual(expected);
+        })
+        .then((results) => console.log(results));
+      denoPact.pactFfi().$$close();
+    });
+    it("should return report on errors", async () => {
+      pactFile.interactions[0].request.path = "/getNotMyModel";
+
+      const expected = JSON.parse(
+        decoder.decode(
+          await Deno.readFile(
+            path.join(getModuleDir(import.meta), "validationFailure.json")
+          )
+        )
+      );
+
+      const requestData = {
+        oas: {
+          content: oasDefinition
+        },
+        pact: {
+          content: pactFile
+        }
+      };
+
+      const denoPact = new DenoPact();
+      await denoPact
+        .setupLoggers(Pact.LevelFilter.LevelFilter_Info)
+        // Arrange
+        .newPact(
+          "swagger-mock-validator-consumer",
+          "swagger-mock-validator-provider"
+        )
+        .addMetaDataToPact(denoPact.getPactFfiVersion())
+        .newInteraction("A OpenAPI/Pact request")
+        .given("an OpenAPI definition exists")
+        .uponReceiving("an HTTP request that doesnt in the OpenAPI definition")
+        .setPactSpecification(Pact.PactSpecification.PactSpecification_V4)
+        .withRequest("/", "POST")
+        .withResponse(200)
+        .withHeader(
+          Pact.InteractionPart.InteractionPart_Request,
+          "content-type",
+          "application/json"
+        )
+        .withBody(
+          Pact.InteractionPart.InteractionPart_Request,
+          "application/json",
+          requestData
+        )
+        .withBody(
+          Pact.InteractionPart.InteractionPart_Response,
+          "application/json",
+          expected
+        )
+        .createMockServerForTransport("http", "0.0.0.0", 0)
+
+        .executeTest(async () => {
+          // Act
+          const response = await fetch(
+            `http://localhost:${denoPact.getMockServerPort()}`,
+            {
+              method: "POST",
+              body: JSON.stringify(requestData),
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              }
+            }
+          );
+          // Assert
+          const actual = await response.json();
+          expect(actual).toEqual(expected);
+        })
+        .then((results) => console.log(results));
+      denoPact.pactFfi().$$close();
+    });
+  });
+});
+
+run();
