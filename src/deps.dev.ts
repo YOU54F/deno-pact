d728074f8254e17f542cdcd20b33c6d8f02742c5
Yousaf Nabi <you@saf.dev>
2022-12-08 21:56:14 +0000

    chore: quick way to test release

--- src/deps.dev.ts
+++ src/deps.dev.ts
@@ -1,7 +1,14 @@
-export { DenoPact } from "./mod.ts";
-export { getModuleDir } from "./mod.ts";
-export { Pact } from "./mod.ts";
+export { DenoPact, Pact, getModuleDir } from "./mod.ts";
 
 export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 export { isPortAvailableSync } from "https://deno.land/x/port/mod.ts";
 export * as path from "https://deno.land/std/path/mod.ts";
+
+
+// // Comment the following in, and the above out, to quickly test
+
+// export { DenoPact, Pact } from "https://deno.land/x/pact/src/mod.ts";
+// export { getModuleDir } from "https://deno.land/x/pact/src/lib/utils.ts";
+// export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+// export { isPortAvailableSync } from "https://deno.land/x/port/mod.ts";
+// export * as path from "https://deno.land/std/path/mod.ts";
